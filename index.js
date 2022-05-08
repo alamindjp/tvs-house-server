bd1ddfdf87b2c5045ecec5e88648f65805bbb5be
alamindjp <alaminhossaindjp@gmail.com>
2022-05-09 05:18:13 +0600

    create backend, install express cors mongodb dotenv, add middleware, connect mongodb,create env file, create app, load data and update somethings

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7wkdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("tvsHouse").collection("product");
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product)
+        });
+    }
+    catch (error) {
+        console.log(error)
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send(`<h2>running tvs house server</h2>`)
+});
+
+app.listen(port, () => {
+    console.log('listening to port', port)
+})
